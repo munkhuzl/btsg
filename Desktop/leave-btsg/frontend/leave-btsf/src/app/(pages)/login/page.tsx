c4c7579829d54be29cd0b05195f1e870e72bc07e
Odonkhuu Munkhzul <bolzop.zulaa@gmail.com>
2025-01-30 22:32:35 +0800

    get

--- Desktop/leave-btsg/frontend/leave-btsf/src/app/(pages)/login/page.tsx
+++ Desktop/leave-btsg/frontend/leave-btsf/src/app/(pages)/login/page.tsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
 
   const login = async (values: FormValues) => {
     try {
-      const response = await fetch(`http://localhost:3000/login`, {
+      const response = await fetch(`http://localhost:4001 /login`, {
         method: "POST",
         headers: {
           "Content-type": "application/json; charset=UTF-8",
